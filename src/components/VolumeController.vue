d9828d0f6d29dd43ccc424f5a11067684754a84e
ali4zimi <alibabaazimi@gmail.com>
2023-06-06 19:02:19 +0200

    volume slider fixed

--- src/components/VolumeController.vue
+++ src/components/VolumeController.vue
@@ -1,8 +1,21 @@
 <script setup>
+import { ref } from 'vue'
 import VolumeHigh from './icons/VolumeHigh.vue'
 import VolumeMedium from './icons/VolumeMedium.vue'
 import VolumeLow from './icons/VolumeLow.vue'
 import VolumeMute from './icons/VolumeMute.vue'
+
+const volumeLevel = ref(100);
+const slider = ref(null);
+const sliderFill = ref(null);
+
+const updateVolume = (e) => {
+    const value = e.target.value;
+    const sliderWidth = slider.value.offsetWidth;
+    const progress = (value / 100) * sliderWidth;
+    sliderFill.value.style.width = `${progress}px`;
+    
+}
 
 </script>
 
@@ -11,11 +24,13 @@ import VolumeMute from './icons/VolumeMute.vue'
         <div class="volume-button">
             <VolumeHigh height="20" width="20" />
         </div>
-        <div class="volume-controller ">
-            <div class="volume-slider">
-                <div class="volume-slider-fill"></div>
+        <div ref="volumeLevel" class="volume-level">
+            <div class="volume-slider-wrapper">
+                <div ref="slider" class="volume-slider">
+                    <div ref="sliderFill" class="volume-slider-fill"></div>
+                </div>
             </div>
-            <input type="range" />
+            <input class="volume-range" type="range" @input="updateVolume" />
         </div>
     </div>
 </template>
@@ -29,9 +44,23 @@ import VolumeMute from './icons/VolumeMute.vue'
 
 }
 
-.volume-slider {
-    @apply flex items-center
+.volume-level {
+    @apply  w-[150px] relative flex items-center h-[15px]
 }
 
+.volume-slider-wrapper {
+    @apply w-full relative px-[8px]
+}
+.volume-slider {
+    @apply w-full h-1 bg-slate-300 rounded-md
+}
+
+.volume-slider-fill {
+    @apply h-1 bg-slate-50 rounded-md
+}
+
+.volume-range {
+    @apply w-full absolute top-0 opacity-0
+}
 
 </style>
