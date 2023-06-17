5fa2ed911dd342ae5a8c62a6af7fbf8a9e647803
ali4zimi <alibabaazimi@gmail.com>
2023-06-17 20:40:26 +0200

    fix: like and unliking track

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -144,7 +144,7 @@ onMounted(() => {
                 <div v-else>Unknown</div>
             </div>
             <div>
-                <div class="like-icon text-red-400" v-if="playlist.tracks.includes(player.currentTrack)" @click="user.unlikeSong(playlist.tracks.indexOf(player.currentTrack), 1)">
+                <div class="like-icon text-red-400" v-if="playlist.tracks.includes(player.currentTrack)" @click="user.unlikeSong(player.currentTrack)">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
                         <path fill="currentColor"
                             d="m12 21l-1.45-1.3q-2.525-2.275-4.175-3.925T3.75 12.812Q2.775 11.5 2.388 10.4T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.3 0 2.475.55T12 4.75q.85-1 2.025-1.55t2.475-.55q2.35 0 3.925 1.575T22 8.15q0 1.15-.388 2.25t-1.362 2.412q-.975 1.313-2.625 2.963T13.45 19.7L12 21Z" />
