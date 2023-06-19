b0fa26a16441a0b0b78cbd6ce20deaaa05ccb00b
ali4zimi <alibabaazimi@gmail.com>
2023-06-19 22:42:37 +0200

    edited z-index of main content

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ import Sidebar from './components/Sidebar.vue'
 
 <style scoped>
 main {
-  @apply fixed top-[80px] bottom-[90px] left-[10px] right-[10px] z-10 flex gap-2;
+  @apply fixed top-[80px] bottom-[90px] left-[10px] right-[10px] flex gap-2;
 }
 .content {
   @apply w-9/12  bg-slate-100 rounded-md overflow-x-hidden overflow-y-scroll;
